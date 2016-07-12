d07c986082ee82bd0fb1d0133cad27fc486da67e
johnteske <john.teske@gmail.com>
2016-07-11 19:57:13 -0700

    Add initial forest show details

---
+++ performances/forest-show-6/index.php
@@ -0,0 +1,119 @@
+<?php
+	$title = 'Forest Show';
+	$class = 'perf';
+	include '../../header.php';?>
+<!-- content -->
+					<div class="row">
+						<header class="2u 3u(2) 12u$(4) date">
+ 							<h3>Jul 30, 2016</h3>
+							<p>7:30&ndash;8:46 pm
+							</p>
+						</header>
+						<section class="7u$ 9u$(2) 12u$(4)">
+						<header>
+							<h3>secret forest show
+							</h3>						
+							<p>
+								John Teske and Neil Welch
+							</p>
+						</header>
+							<p>
+Neil Welch and I present our sixth 'secret forest show' among the trees in Seattle.
+
+John will present a version of <em>nomadic patterns</em>, where he leads the performer-audience in an exploratory sonic investigation of the environment.
+
+Neil will present a solo piece, followed by an ensemble piece with
+Kate Olson, Ivan Arteaga, Evan Smith, Heather Bentley, Aria Peters, David Balatero, John Teske, Janna Webbon, Sarah Pyle, and Jen Gilleran.
+							</p>
+							<p>
+								Seattle, WA
+							</p>
+						</section>
+					</div> 
+<style>
+	.centerd {
+		text-align: center;
+		font-style: italic;
+		margin-bottom: 1em;
+	}
+	.forest h3 {
+		margin: 0 0 0.5em 0;
+	}
+</style>
+<h2>Location and Directions</h2>
+<div class="row centerd">
+	<div class="-4u 4u$">
+		<img src="../forest-show-5/grey.jpg" class="image fit">
+		<p>The walk takes about 15&nbsp;minutes.</p>
+	</div>
+</div>
+<div class="forest">
+	<div class="row">
+		<h3 class="6u 12u(3)">Begin at the Cowen Park entrance on <a href="https://goo.gl/maps/XDLt7" target="_blank" title="View on Google maps" style="text-decoration: underline;">NE 61st and Brooklyn Ave NE</a></h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-1sm.jpg" class="image fit">
+		</div>
+	
+		<h3 class="6u 12u(3)">Take a left at the sign, head down into the ravine</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-2sm.jpg" class="image fit">
+		</div>
+	
+		<h3 class="6u 12u(3)">Continue straight on the trail, pass under the 15th Ave bridge</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-3sm.jpg" class="image fit">
+		</div>
+	</div>
+	<div class="row centerd">
+		<div class="-4u 4u$">
+			<img src="../forest-show-5/cloud.png" class="image fit">
+			<p>Listen to the path.</p>
+		</div>
+	</div>
+	<div class="row">	
+		<h3 class="6u 12u(3)">Continue straight across the footbridge</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-4sm.jpg" class="image fit">
+		</div>
+		
+		<h3 class="6u 12u(3)">Pass the wooden steps on the left</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-5sm.jpg" class="image fit">
+		</div>
+		
+		<h3 class="6u 12u(3)">Notice the clearing to the left</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-6sm.jpg" class="image fit">
+		</div>
+	</div>
+	<div class="row centerd">
+		<div class="-4u 4u$">
+			<img src="../forest-show-5/phrase.png" class="image fit">
+		</div>
+	</div>	
+	<div class="row">	
+		<h3 class="6u 12u(3)">Enter, walk toward the wooden bridge</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-7sm.jpg" class="image fit">
+		</div>
+		
+		<h3 class="6u 12u(3)">Cross the small stream</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-8sm.jpg" class="image fit">
+		</div>
+		
+		<h3 class="6u 12u(3)">Take a left into the performance area</h3>
+		<div class="6u 12u(3)">
+			<img src="../forest-show-5/150718-9sm.jpg" class="image fit">
+		</div>
+	</div>
+	<div class="row centerd">
+		<div class="-4u 4u$">
+			<img src="../forest-show-5/stacc.png" class="image fit">
+			<!-- <p>You will be given a gift to care for.</p> -->
+		</div>
+	</div>
+</div>
+<!-- content -->
+<?php $photo = 'Photo by Blake Bumpus.';
+	include '../../footer.php';?>
