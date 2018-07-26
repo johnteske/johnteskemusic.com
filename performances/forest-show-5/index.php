ab5b8be20b7aea77703b251391ab637ac402b5b4
John Teske <john.teske@gmail.com>
2018-07-25 23:12:02 -0700

    Use shared logic for past forest shows

--- performances/forest-show-5/index.php
+++ performances/forest-show-5/index.php
@@ -1,30 +1,33 @@
 <?php
+	include '../forest-show-ravenna.php';
+
 	$title = 'Forest Show';
 	$class = 'perf';
 	include '../../header.php';?>
-<!-- content -->
-					<div class="row">
-						<header class="2u 3u(2) 12u$(4) date">
- 							<h3>Jul 18, 2015</h3>
-							<p>8&ndash;9 pm
-							</p>
-						</header>
-						<section class="7u$ 9u$(2) 12u$(4)">
-						<header>
-							<h3>secret forest show
-							</h3>						
-							<p>
-								with Neil Welch
-							</p>
-						</header>
-							<p>
-								Neil Welch and I present our fifth 'secret forest show' among the trees in Seattle. This year we will have a more interactive and site-specific performance, featuring works by Neil and myself. We'll be joined by Daria Binkowski, Ivan Arteaga, Rose Bellini, Chris Icasiano, and Greg and Tom Campbell.
-							</p>
-							<p>
-								Seattle, WA
-							</p>
-						</section>
-					</div> 
+
+<div class="row">
+	<header class="2u 3u(2) 12u$(4) date">
+		<h3>Jul 18, 2015</h3>
+		<p>8&ndash;9 pm
+		</p>
+	</header>
+	<section class="7u$ 9u$(2) 12u$(4)">
+	<header>
+		<h3>secret forest show
+		</h3>
+		<p>
+			with Neil Welch
+		</p>
+	</header>
+		<p>
+			Neil Welch and I present our fifth 'secret forest show' among the trees in Seattle. This year we will have a more interactive and site-specific performance, featuring works by Neil and myself. We'll be joined by Daria Binkowski, Ivan Arteaga, Rose Bellini, Chris Icasiano, and Greg and Tom Campbell.
+		</p>
+		<p>
+			Seattle, WA
+		</p>
+	</section>
+</div>
+
 <style>
 	.centerd {
 		text-align: center;
@@ -35,6 +38,7 @@
 		margin: 0 0 0.5em 0;
 	}
 </style>
+
 <h2>Location and Directions</h2>
 <div class="row centerd">
 	<div class="-4u 4u$">
@@ -44,20 +48,26 @@
 </div>
 <div class="forest">
 	<div class="row">
-		<h3 class="6u 12u(3)">Begin at the Cowen Park entrance on <a href="https://goo.gl/maps/XDLt7" target="_blank" title="View on Google maps" style="text-decoration: underline;">NE 61st and Brooklyn Ave NE</a></h3>
-		<div class="6u 12u(3)">
-			<img src="150718-1sm.jpg" class="image fit">
-		</div>
-	
-		<h3 class="6u 12u(3)">Take a left at the sign, head down into the ravine</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-2sm.jpg" class="image fit">
-		</div>
-	
-		<h3 class="6u 12u(3)">Continue straight on the trail, pass under the 15th Ave bridge</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-3sm.jpg" class="image fit">
-		</div>
+		<?php
+			$map_directions = 'Begin at the Cowen Park entrance on ' .
+				'<a href="https://goo.gl/maps/XDLt7" target="_blank" title="View on Google maps" style="text-decoration: underline;">' .
+					'NE 61st and Brooklyn Ave NE' .
+				'</a>';
+		?>
+		<?php echo_direction(
+			$map_directions,
+			'150718-1.jpg')
+		?>
+
+		<?php echo_direction(
+			'Take a left at the sign, head down into the ravine',
+			'150718-2.jpg')
+		?>
+
+		<?php echo_direction(
+			'Continue straight on the trail, pass under the 15th Ave bridge',
+			'150718-3.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
@@ -66,41 +76,41 @@
 		</div>
 	</div>
 	<div class="row">	
-		<h3 class="6u 12u(3)">Continue straight across the footbridge</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-4sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Pass the wooden steps on the left</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-5sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Notice the clearing to the left</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-6sm.jpg" class="image fit">
-		</div>
+		<?php echo_direction(
+			'Continue straight across the footbridge',
+			'150718-4.jpg')
+		?>
+
+		<?php echo_direction(
+			'Pass the wooden steps on the left',
+			'150718-5.jpg')
+		?>
+
+		<?php echo_direction(
+			'Notice the clearing to the left',
+			'150718-6.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
 			<img src="phrase.png" class="image fit">
 		</div>
-	</div>	
-	<div class="row">	
-		<h3 class="6u 12u(3)">Enter, walk toward the wooden bridge</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-7sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Cross the small stream</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-8sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Take a left into the performance area</h3>
-		<div class="6u 12u(3)">
-			<img src="150718-9sm.jpg" class="image fit">
-		</div>
+	</div>
+	<div class="row">
+		<?php echo_direction(
+			'Enter, walk toward the wooden bridge',
+			'150718-7.jpg')
+		?>
+
+		<?php echo_direction(
+			'Cross the small stream',
+			'150718-8.jpg')
+		?>
+
+		<?php echo_direction(
+			'Take a left into the performance area',
+			'150718-9.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
@@ -109,6 +119,6 @@
 		</div>
 	</div>
 </div>
-<!-- content -->
+
 <?php $photo = 'Photo by Blake Bumpus.';
 	include '../../footer.php';?>
--- performances/forest-show-6/index.php
+++ performances/forest-show-6/index.php
@@ -1,34 +1,37 @@
 <?php
+	include '../forest-show-ravenna.php';
+
 	$title = 'Forest Show';
 	$class = 'perf';
 	include '../../header.php';?>
-<!-- content -->
-					<div class="row">
-						<header class="2u 3u(2) 12u$(4) date">
- 							<h3>Jul 30, 2016</h3>
-							<p>7:30&ndash;8:46 pm
-							</p>
-						</header>
-						<section class="7u$ 9u$(2) 12u$(4)">
-						<header>
-							<h3>secret forest show
-							</h3>						
-							<p>
-								John Teske and Neil Welch
-							</p>
-						</header>
-							<p>
+
+<div class="row">
+	<header class="2u 3u(2) 12u$(4) date">
+		<h3>Jul 30, 2016</h3>
+		<p>7:30&ndash;8:46 pm
+		</p>
+	</header>
+	<section class="7u$ 9u$(2) 12u$(4)">
+	<header>
+		<h3>secret forest show
+		</h3>
+		<p>
+			John Teske and Neil Welch
+		</p>
+	</header>
+		<p>
 Neil Welch and I present our sixth 'secret forest show' among the trees in Seattle.
 
 John will present a version of <em>nomadic patterns</em>, where he leads the performer-audience in an exploratory sonic investigation of the environment.
 
 Neil will present a solo piece, followed by <em>Concepcion Picciotto</em>, for 10 musicians, birds, and orator. Featuring Kate Olson, Ivan Arteaga, Evan Smith, Heather Bentley, Aria Peters, David Balatero, John Teske, Janna Webbon, Sarah Pyle, and Jen Gilleran.
-							</p>
-							<p>
-								Seattle, WA
-							</p>
-						</section>
-					</div> 
+		</p>
+		<p>
+			Seattle, WA
+		</p>
+	</section>
+</div>
+
 <style>
 	.centerd {
 		text-align: center;
@@ -39,8 +42,9 @@ Neil will present a solo piece, followed by <em>Concepcion Picciotto</em>, for 1
 		margin: 0 0 0.5em 0;
 	}
 </style>
-<link rel="stylesheet" href="glob/score.css">
-<link rel="stylesheet" href="glob/styles.css">
+
+<!-- <link rel="stylesheet" href="glob/score.css"> -->
+<!-- <link rel="stylesheet" href="glob/styles.css"> -->
 
 <h2>Location and Directions</h2>
 <div class="row centerd">
@@ -51,43 +55,49 @@ Neil will present a solo piece, followed by <em>Concepcion Picciotto</em>, for 1
 </div>
 <div class="forest">
 	<div class="row">
-		<h3 class="6u 12u(3)">Begin at the Cowen Park entrance on <a href="https://goo.gl/maps/XDLt7" target="_blank" title="View on Google maps" style="text-decoration: underline;">NE 61st and Brooklyn Ave NE</a></h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-1sm.jpg" class="image fit">
-		</div>
-	
-		<h3 class="6u 12u(3)">Take a left at the sign, head down into the ravine</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-2sm.jpg" class="image fit">
-		</div>
-	
-		<h3 class="6u 12u(3)">Continue straight on the trail, pass under the 15th Ave bridge</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-3sm.jpg" class="image fit">
-		</div>
+	<?php
+			$map_directions = 'Begin at the Cowen Park entrance on ' .
+				'<a href="https://goo.gl/maps/XDLt7" target="_blank" title="View on Google maps" style="text-decoration: underline;">' .
+					'NE 61st and Brooklyn Ave NE' .
+				'</a>';
+		?>
+		<?php echo_direction(
+			$map_directions,
+			'150718-1.jpg')
+		?>
+
+		<?php echo_direction(
+			'Take a left at the sign, head down into the ravine',
+			'150718-2.jpg')
+		?>
+
+		<?php echo_direction(
+			'Continue straight on the trail, pass under the 15th Ave bridge',
+			'150718-3.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
-	        <svg class="main image fit"></svg>
-<!-- 			<img src="../forest-show-5/cloud.png" class="image fit"> -->
+	        <!-- <svg class="main image fit"></svg> -->
+			<img src="../forest-show-5/cloud.png" class="image fit">
 			<p>Listen to the path.</p>
 		</div>
 	</div>
 	<div class="row">	
-		<h3 class="6u 12u(3)">Continue straight across the footbridge</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-4sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Pass the wooden steps on the left</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-5sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Notice the clearing to the left</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-6sm.jpg" class="image fit">
-		</div>
+	<?php echo_direction(
+			'Continue straight across the footbridge',
+			'150718-4.jpg')
+		?>
+
+		<?php echo_direction(
+			'Pass the wooden steps on the left',
+			'150718-5.jpg')
+		?>
+
+		<?php echo_direction(
+			'Notice the clearing to the left',
+			'150718-6.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
@@ -95,31 +105,29 @@ Neil will present a solo piece, followed by <em>Concepcion Picciotto</em>, for 1
 		</div>
 	</div>	
 	<div class="row">	
-		<h3 class="6u 12u(3)">Enter, walk toward the wooden bridge</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-7sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Cross the small stream</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-8sm.jpg" class="image fit">
-		</div>
-		
-		<h3 class="6u 12u(3)">Take a left into the performance area</h3>
-		<div class="6u 12u(3)">
-			<img src="../forest-show-5/150718-9sm.jpg" class="image fit">
-		</div>
+		<?php echo_direction(
+			'Enter, walk toward the wooden bridge',
+			'150718-7.jpg')
+		?>
+
+		<?php echo_direction(
+			'Cross the small stream',
+			'150718-8.jpg')
+		?>
+
+		<?php echo_direction(
+			'Take a left into the performance area',
+			'150718-9.jpg')
+		?>
 	</div>
 	<div class="row centerd">
 		<div class="-4u 4u$">
 			<img src="../forest-show-5/stacc.png" class="image fit">
-			<!-- <p>You will be given a gift to care for.</p> -->
 		</div>
 	</div>
 </div>
-<script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
-<script src="glob/script.js" charset="utf-8"></script>
+<!-- <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script> -->
+<!-- <script src="glob/script.js" charset="utf-8"></script> -->
 
-<!-- content -->
 <?php $photo = 'Photo by Blake Bumpus.';
 	include '../../footer.php';?>
