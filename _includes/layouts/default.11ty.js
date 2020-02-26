8f1db55af08b83b5306a55239522f5c6fff38c03
John Teske <john.teske@gmail.com>
2020-02-26 08:00:25 -0800

    Set up eleventy structure

---
+++ _includes/layouts/default.11ty.js
@@ -0,0 +1,28 @@
+const title = data =>
+  [data.title, data.project, data.site.name].filter(Boolean).join(" - ");
+
+module.exports = data =>
+  `<!DOCTYPE html>
+<html>
+  <head>
+    <title>${title(data)}</title>
+  </head>
+  <body>
+    <header>
+      <a href="/"><h1>${data.site.name}</h1></a>
+    </header>
+    <nav>
+      <a href="/performances">performances</a>
+      <a href="/compositions">compositions</a>
+      <a href="/about">about</a>
+    </nav>
+    <main>
+      ${data.content}
+    </main>
+    <footer>
+      <ul>
+        ${data.contact.map(v => `<li><a href="${v.url}">${v.label}</a></li>`).join("")}
+      </ul>
+    </footer>
+  </body>
+</html>`;
--- performances/forest-show-ravenna.php
+++
@@ -1,9 +0,0 @@
-<?php
-$image_path = '../../images/forest-show-directions/ravenna-park/';
-
-function echo_direction($text, $image_basename) {
-    global $image_path;
-    echo '<h3 class="6u 12u(3)">' . $text . '</h3>' .
-        '<div class="6u 12u(3)"><img src="' . $image_path . $image_basename . '" class="image fit"></div>';
-}
-?>
