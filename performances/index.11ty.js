db71c055c4e2a862764e4dbb342686bd13a42213
John Teske <john.teske@gmail.com>
2020-04-21 22:54:13 -0700

    Refine date comparison

--- performances/index.11ty.js
+++ performances/index.11ty.js
@@ -2,14 +2,9 @@ const { chunk } = require("lodash");
 const moment = require("moment");
 const { maybe } = require("../util");
 
-const now = moment().format("YYYY-MM-DD");
-
-// TODO date is in JSON, in ISO format, and
-// does not need to be parsed from the filename
-const date = yymmdd =>
-  `20${chunk(yymmdd, 2)
-    .map(v => v.join(""))
-    .join("-")}`;
+const format = "YYYY-MM-DD";
+const now = moment().format(format);
+const date = yymmdd => moment("20" + yymmdd).format(format);
 
 module.exports = class {
   data() {
@@ -24,7 +19,6 @@ module.exports = class {
           const _date = date(key.split("-")[0]);
           return `<li>
             ${_date}
-            ${now}
             ${p.title}${
             moment(_date).isAfter(now)
               ? `<br />${p.time}<br /><a href="${p.url}">${p.address}</a>`
