8af9e5881d9c1c1d97b24780a3ad73f5932f62de
John Teske <john.teske@gmail.com>
2020-05-03 19:33:18 -0700

    Use ffdb-jtm for performance data

--- performances/index.11ty.js
+++ performances/index.11ty.js
@@ -4,23 +4,22 @@ const { maybe } = require("../util");
 
 const format = "YYYY-MM-DD";
 const now = moment().format(format);
-const date = yymmdd => moment("20" + yymmdd).format(format);
+const date = yyyymmdd => moment(yyyymmdd).format(format);
 
 module.exports = class {
   data() {
     return { title: "Performances" };
   }
-  render(data) {
+  async render(data) {
+    const performances = await data.performances
     return `<ul>
-      ${Object.keys(data.performances)
+      ${performances
         .reverse()
-        .map(key => {
-          const p = data.performances[key];
-          const _date = date(key.split("-")[0]);
+        .map(p => {
           return `<li>
-            ${_date}
+            ${date(p.date)}
             ${p.title}${
-            moment(_date).isAfter(now)
+            moment(p.date).isAfter(now)
               ? `<br />${p.time}<br /><a href="${p.url}">${p.address}</a>`
               : ""
           }
